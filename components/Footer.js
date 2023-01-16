ea6bc1e2812ad8337e5c77fb2f4e7c4da0737d55
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2023-01-17 01:17:09 +0500

    0.2.1 - Layout fix

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,11 @@ import Total from "@/components/Total";
 
 export default function Footer() {
     return (
-        <div className={'fixed bottom-0 left-0 bg-white'}>
-            <Total/>
-            <NewItem/>
+        <div className={'fixed bottom-0 left-0 bg-white w-full'}>
+            <div className={'max-w-lg mx-auto'}>
+                <Total/>
+                <NewItem/>
+            </div>
         </div>
     )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 export default function Layout({children}) {
     return (
         <>
-            <main className={'pb-52'}>
+            <main className={'pb-52 max-w-lg mx-auto'}>
                 {children}
             </main>
             <Footer/>
