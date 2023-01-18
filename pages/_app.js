4426f95b4b434892e6f0664c09652faf981122c8
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2023-01-18 23:53:38 +0500

    0.4.1 - Meta tag added

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
         <Provider store={store}>
             <Head>
                 <title>Shopping list</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <Layout>
                 <Component {...pageProps} />
