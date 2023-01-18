9e9a2946faeb5b1ca1d5e5ae5d70bd754d88ed65
lil-m00ha <anastasiia.n.minaeva@gmail.com>
2023-01-18 23:43:26 +0500

    0.4.0 - Layout change
    - Main part and Footer are now in flex container with column order;
    - Main part has overflow now which allows for Cart items section not to 'hide' up the page;

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,9 @@ import Total from "@/components/Total";
 
 export default function Footer() {
     return (
-        <div className={'fixed bottom-0 left-0 bg-white w-full border-t-2 border-dashed border-t-lime-300 '}>
-            <div className={'max-w-lg mx-auto'}>
-                <Total/>
-                <NewItem/>
-            </div>
+        <div className={'bg-white w-full border-t-2 border-dashed border-t-lime-300 '}>
+            <Total/>
+            <NewItem/>
         </div>
     )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,11 @@ import Footer from "@/components/Footer";
 
 export default function Layout({children}) {
     return (
-        <>
-            <main className={'px-4 pt-4 pb-72 max-w-lg mx-auto'}>
+        <div className={'flex flex-col h-screen max-w-lg mx-auto'}>
+            <main className={'p-4 grow overflow-scroll'}>
                 {children}
             </main>
             <Footer/>
-        </>
+        </div>
     )
 }
